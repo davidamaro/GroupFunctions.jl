26a46c80b61350e418aa88bd7e1a12edec07285f
David Amaro Alcalá <dav1494@ciencias.unam.mx>
2020-09-14 23:38:53 -0500

    arreglados (y añadidos) tipos de perm.jl

--- src/generic/PermGroups.jl
+++ src/generic/PermGroups.jl
@@ -13,41 +13,33 @@ true
 function descomp_total(original::Perm)
     lista_ciclos = cycles(original)
     #@show lista_ciclos
-    completa = []
+    completa = Tuple{Int64,Int64}[]
     for elem in lista_ciclos
-        #push!(completa, descomponer_ciclo(elem))
         descomponer_ciclo!(elem, completa)
     end
     completa
 end
 
-function descomponer_ciclo!(ciclo, output)
+function descomponer_ciclo!(ciclo::SubArray{Int64,1,Array{Int64,1},Tuple{UnitRange{Int64}},true}, output::Array{Tuple{Int64,Int64},1})
     lista = Tuple{Int64,Int64}[]
     for i in 1:length(ciclo) - 1
         push!(lista, (ciclo[i], ciclo[i+1]))
     end
-    #lista
-    #output = Tuple{Int64,Int64}[]
+
     for elem in lista
         individual!(elem, output)
     end
-    #output
 end
 
-function individual!(transp,lista)
-    j,k = transp
+function individual!(tranposicion::Tuple{Int64,Int64},lista::Array{Tuple{Int64,Int64},1})
+    j,k = tranposicion
     if k < j
       j,k = k,j
     end
     for i in k:-1:j+1
-        #@show (i-1, i)
         push!(lista, (i-1,i))
-        #prepend!(lista, (i-1,i))
     end
-    #println("luego")
     for i in j+1:k-1
-        #@show (i,i+1)
         push!(lista, (i, i+1))
-        #prepend!(lista, (i, i+1))
     end
 end
