7a65ae8ae435e27afa74888464c9a0af7c03f213
David Amaro Alcala <dav1494@ciencias.unam.mx>
2024-12-21 12:13:24 -0600

    wrapper

--- src/GroupFunctions.jl
+++ src/GroupFunctions.jl
@@ -1,20 +1,45 @@
-module GroupFunctions
+# module GroupFunctions
 
+# using Markdown
+# #import AbstractAlgebra: YoungTableau
+# using AbstractAlgebra
+# import AbstractAlgebra: YoungTableau
+
+# import SparseArrays: spzeros, SparseMatrixCSC
+# import SymEngine: expand
+
+# #greet() = print("Hello World!")
+# include("generic/GTPatterns.jl")
+# include("generic/YoungTabs.jl")
+# include("generic/DF.jl")
+# include("generic/PermGroups.jl")
+# include("generic/Misc.jl")
+
+# #import GTPattern, basis_states
+# export YoungTableau, axialdistance, encontrar_posicion
+# export GTPattern, basis_states, siguientepatron, siguientepatron!
+# export primero_lexi, StandardYoungTableaux, generar_matriz
+# export indice_tablon_semistandard, content, Θ
+# export group_function, @mma_str, mma_to_julia
+# export zweight, pweight
+# export gtinicial
+# export generar_matriz
+# export bloquesun, simplefactorization, simple
+# export expand
+
+# end # module
+module GroupFunctions
 using Markdown
-#import AbstractAlgebra: YoungTableau
 using AbstractAlgebra
 import AbstractAlgebra: YoungTableau
-using SymEngine
-
 import SparseArrays: spzeros, SparseMatrixCSC
-
+using SymEngine: expand  # Changed this line
 #greet() = print("Hello World!")
 include("generic/GTPatterns.jl")
 include("generic/YoungTabs.jl")
 include("generic/DF.jl")
 include("generic/PermGroups.jl")
 include("generic/Misc.jl")
-
 #import GTPattern, basis_states
 export YoungTableau, axialdistance, encontrar_posicion
 export GTPattern, basis_states, siguientepatron, siguientepatron!
@@ -25,5 +50,5 @@ export zweight, pweight
 export gtinicial
 export generar_matriz
 export bloquesun, simplefactorization, simple
-
+export expand
 end # module
